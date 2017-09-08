9ff841d88f8c99502a1b796f8dc2dae9c98248a1
root <root@ZBXslave.(none)>
2017-09-08 10:18:07 +0800

    rebuild

--- public/app/plugins/datasource/grafana/datasource.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../../../../public/app/headers/common.d.ts" />
-declare class GrafanaDatasource {
-    private backendSrv;
-    /** @ngInject */
-    constructor(backendSrv: any);
-    query(options: any): any;
-    annotationQuery(options: any): any;
-}
-export { GrafanaDatasource };
